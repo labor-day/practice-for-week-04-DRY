5c18df7fdd6f16de113a1af627513323313c2117
labor-day <shaffer.scott@yahoo.com>
2022-06-23 12:12:56 -0600

    DRY

--- practice.js
+++ practice.js
@@ -1,45 +1,26 @@
-function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
+function compare(num1, num2, preference) {
+
+  if (preference === "larger") {
+    return Math.max(num1, num2);
   } else {
-    bigNum = num2;
-    return bigNum * 2;
+    return Math.min(num1, num2);
   }
+}
+
+function multiplyBiggerNumByTwo(num1, num2) {
+  return compare(...arguments, "larger") * 2;
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  return compare(...arguments, "larger") / 3;
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+    return `I ate ${compare(...arguments, "larger")} tacos.`;
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+    return `I adopted a dog that weighs ${compare(...arguments, "smaller")} pounds.`;
 }
 
 
